b34144147728612df52d1f07f37bd47ac82072ff
Steven Li <lsich@bu.edu>
2025-07-27 20:33:53 -0400

    added paddig on top

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 export default function Header() {
     return (
-        <header className="flex flex-col justify-center items-center h-20 px-4 bg-black">
+        <header className="flex flex-col justify-center items-center h-20 px-4 bg-black pt-10">
             <h1 className="text-4xl font-semibold text-white">Cats!</h1>
             <h2 className="text-6xl font-semibold text-white">(Refresh to See New Cats)</h2>
         </header>
