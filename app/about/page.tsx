b004eaf4dd343d8c9c9d49ee2f890b0e4cc68cda
Steven Li <lsich@bu.edu>
2025-07-27 20:40:25 -0400

    added text scalig in the about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Page() {
     return (
         <main className="flex flex-col items-center justify-center h-screen text-center">
-            <p className="text-2xl mb-6">This is a project using The Cat API with Next.js.</p>
+            <p className= "text-2xl sm:text-4sm md:text-l lg:text-xl mb-6">This is a project using The Cat API with Next.js.</p>
             <Link
                 href="/"
                 className="text-white font-semibold text-lg px-6 py-3 rounded-lg shadow-md hover:underline"
