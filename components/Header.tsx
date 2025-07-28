8ebe82bc492b0903bba0754cc3599065fd3dab1f
Steven Li <lsich@bu.edu>
2025-07-27 20:37:11 -0400

    added scaling to header text

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 export default function Header() {
     return (
         <header className="flex flex-col justify-center items-center h-20 px-4 bg-black pt-10">
-            <h1 className="text-4xl font-semibold text-white">Cats!</h1>
-            <h2 className="text-6xl font-semibold text-white">(Refresh to See New Cats)</h2>
+            <h1 className="text-3xl sm:text-4xl md:text-5xl font-semibold text-white">Cats!</h1>
+            <h2 className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold text-white">(Refresh to See New Cats)</h2>
         </header>
     );
 }
